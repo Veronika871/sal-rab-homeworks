9d62c2eab20a62a1fa67f6bdaece017f77f5f4e7
Veronika_Skvortsova <ivancovav@mail.ru>
2021-11-06 20:12:07 +0300

    task 1.1. done

--- task_1/solution.js
+++ task_1/solution.js
@@ -16,6 +16,7 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
     let message = 'productName' + " " + "за" + " " + 'productPrice' + " " + "теперь в корзине";
+    
     // Конец решения задания №1.1.
 
     return message;
